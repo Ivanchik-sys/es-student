5f5b9e7a4eefc1b5744b79dd26bdbd3201468a82
Ivan Degtyarenko <degtiarenkoew@yandex.ru>
2026-09-24 19:27:56 +0300

    Сделана п2.1.1

---
+++ 211-command-sub/device/device.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define DEVICE_NAME "es-led-module"
+#define FIRMWARE_VERSION "1.0.0"
+
+#define DEVICE_PROJECT "211-command-sub"
+#define DEVICE_REPO "https://github.com/Ivanchik-sys/es-student"
+
+#ifndef DEVICE_BOARD
+#define DEVICE_BOARD "unknown"
+#endif
+
+void device_info(void);
